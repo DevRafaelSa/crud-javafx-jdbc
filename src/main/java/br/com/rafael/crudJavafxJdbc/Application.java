d90ee089234c4505dabb29ef815254d99ab60d6a
Rafael Sá <sanunesrafael@gmail.com>
2022-09-26 23:40:11 -0300

    About view

--- src/main/java/br/com/rafael/crudJavafxJdbc/Application.java
+++ src/main/java/br/com/rafael/crudJavafxJdbc/Application.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
@@ -16,10 +19,14 @@ public class Application extends javafx.application.Application {
         sc.setFitToHeight(true);
         sc.setFitToWidth(true);
 
-        Scene scene = new Scene(sc);
-        stage.setScene(scene);
+        mainScene = new Scene(sc);
+        stage.setScene(mainScene);
         stage.setTitle("Sample JavaFX application");
         stage.show();
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
